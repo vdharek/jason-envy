b40018db9a0634168dbf1daf7021959aca49d8dd
Vivek Dharek <vdharek@gmail.com>
2024-07-29 15:39:08 +0200

    Coordinate mapping Init.

--- src/envy/AgentModel.java
+++ src/envy/AgentModel.java
@@ -3,26 +3,46 @@ package src.envy;
 import jason.environment.grid.GridWorldModel;
 import jason.environment.grid.Location;
 
+import java.util.Map;
 import java.util.Random;
 
-
+//AgentModel class: For modeling an agents (Agents placement, Agent speed, Agent type, Agent position...)
 public class AgentModel extends GridWorldModel {
-
-    public static final int AGENT = 5;
+    public static final int COORDINATE = 2;
+    //public static final int AGENT = 5;
 
     public AgentModel(int gridsize, int agents) {
         super(gridsize, gridsize, agents);
-        placeAgent(gridsize, agents);
+        //placeAgent(gridsize, agents);
         //setAgPos(0, new Location(gridsize /2, gridsize /2));
 
-        int targetX = Math.min(2, gridsize-1);
-        int targetY = Math.min(2, gridsize-1);
+        //int targetX = Math.min(2, gridsize-1);
+        //int targetY = Math.min(2, gridsize-1);
 
         //add(TARGET, targetX, targetY);
-        addWall(10,20,30,20);
+        //addWall(10,20,30,20);
     }
 
-    public void placeAgent(int gridSize, int agents) {
+    public void setCoordinates(Map<String, String> gridMap) {
+        for (String key : gridMap.keySet()) {
+            String[] parts = key.split(",");
+            int x = Integer.parseInt(parts[0]);
+            int y = Integer.parseInt(parts[1]);
+            add(COORDINATE, x, y);
+            //addWall(x,y,x,y);
+
+        }
+    }
+
+    public void addWall(int x1, int y1, int x2, int y2) {
+        for(int x = x1; x <= x2; ++x) {
+            for(int y = y1; y <= y2; ++y) {
+                add(COORDINATE, x, y);
+            }
+        }
+    }
+
+    /*public void placeAgent(int gridSize, int agents) {
         Random rand = new Random();
         for (int i = 0; i < agents; i++) {
             int x = rand.nextInt(gridSize);
@@ -33,9 +53,9 @@ public class AgentModel extends GridWorldModel {
             }
             setAgPos(i, new Location(x, y));
         }
-    }
+    }*/
 
-    public boolean moveTowards(Location dest) {
+    /*public boolean moveTowards(Location dest) {
         Location agentLocation = getAgPos(0);
         if(agentLocation.equals(dest)) {
             return true;
@@ -48,5 +68,5 @@ public class AgentModel extends GridWorldModel {
         setAgPos(0, newLocation);
         return false;
 
-    }
+    }*/
 }
--- src/envy/AgentView.java
+++ src/envy/AgentView.java
@@ -4,35 +4,72 @@ import jason.environment.grid.GridWorldModel;
 import jason.environment.grid.GridWorldView;
 
 import java.awt.*;
+import java.util.logging.Logger;
 
+//AgentView class: Frame representation. Model graphical objects in the environment.  (
 public class AgentView extends GridWorldView {
+    private static final Logger log = Logger.getLogger(AgentView.class.getName());
+
 
     //AgentModel agentModel;
 
     public AgentView(GridWorldModel model) {
-        super(model, "Envy-sim", 1000);
+        super(model, "Envy-sim", 100);
         setVisible(true);
         repaint();
     }
 
-    public void draw(Graphics g, int x, int y, int object){
+    /*public void draw(Graphics g, int x, int y, int object){
         switch (object){
             case AgentModel.AGENT:
                 drawAgent(g, x, y);
                 break;
         }
-    }
-
-    private void drawAgent(Graphics g, int x, int y) {
-        g.setColor(Color.YELLOW);
-        g.fillOval(x * cellSizeH, y * cellSizeH, cellSizeH, cellSizeH);
-    }
+    }*/
 
     @Override
+    public void draw(Graphics g, int x, int y, int object) {
+        switch (object) {
+            case AgentModel.COORDINATE:
+                //drawCoordinate(g, x, y);
+                //drawAgent(g, x, y);
+                break;
+        }
+    }
+
+
+
+    @Override
+    public void drawObstacle(Graphics g, int x, int y) {
+
+        super.drawObstacle(g, x, y);
+
+    }
+
+    private void drawCoordinate(Graphics g, int x, int y) {
+        g.setColor(Color.BLACK);
+        //g.fillOval();
+        g.drawLine(x * cellSizeH, y * cellSizeH, cellSizeH, cellSizeH);
+        //System.out.println("Coordinates on grid: ");
+        log.info(x * cellSizeH + "," + y * cellSizeH + "," + cellSizeH + "," + cellSizeH);
+    }
+
+    /*private void drawCoordinates(Graphics g, int x, int y){
+        Color c = Color.BLACK;
+        super.drawObstacle(g,x,y);
+    }*/
+
+
+    /*private void drawAgent(Graphics g, int x, int y) {
+        g.setColor(Color.YELLOW);
+        g.fillOval(x * cellSizeH, y * cellSizeH, cellSizeH, cellSizeH);
+    }*/
+
+    /*@Override
     public void drawAgent(Graphics g, int x, int y, Color c, int id) {
         c = Color.BLACK;
         super.drawAgent(g,x,y,c,id);
         g.setColor(Color.RED);
         super.drawString(g,x,y,defaultFont, "A" + (id + 1) + ": Agent");
-    }
+    }*/
 }
